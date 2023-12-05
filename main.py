1585c35e20230c6bbd58dd8b46b5d4dbdce45a01
Nixuge <mail@nixuge.me>
2023-12-05 19:08:46 +0100

    Added dev mode

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from loader.modulesloader import get_all_parsers
 from loader.presets import run_all_parsers, run_multiple_parsers, run_single_parser
 
@@ -7,6 +8,19 @@ FILES = ("duplicates.txt", "ips.txt", "duplicates_bedrock.txt", "ips_bedrock.txt
 
 for file in FILES:
     init_file(file)
+
+if not os.path.isdir("data/"):
+    os.makedirs("data/")
+elif os.path.isfile("data/DEV"):
+    print("DEV MODE - LOADING FILE SPECIFIED IN data/DEV")
+    with open("data/DEV") as file:
+        module_name = f"parsers.{file.read().strip()}"
+    import importlib
+    try:
+        my_module = importlib.import_module(module_name)
+    except Exception as e:
+        print(f"Couldn't import dev module: {e}")
+    exit(0)
 
 all_parsers = get_all_parsers()
 lenP = len(all_parsers)
